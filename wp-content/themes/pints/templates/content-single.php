d5d808a2e55c44f135fe95167ae97fdf2d7431f4
Antonis Lianos <antonis.lianos@impacthub.net>
2014-09-04 20:09:14 +0300

    Style author meta and remove comments

--- wp-content/themes/pints/templates/content-single.php
+++ wp-content/themes/pints/templates/content-single.php
@@ -8,10 +8,7 @@
       <?php the_content(); ?>
     </div>
     <footer>
-	<h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/author-meta'); ?>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
   </article>
 <?php endwhile; ?>
